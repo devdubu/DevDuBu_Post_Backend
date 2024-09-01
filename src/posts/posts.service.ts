2fd0a54d1a0927a215634c256b2e3f170e52431f
진민 <insomnis2020@naver.com>
2024-09-01 22:48:02 +0900

    TypeORM 테스트

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,19 +1,29 @@
-import { Injectable } from '@nestjs/common';
-import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
+import { Injectable } from "@nestjs/common";
+import { CreatePostDto } from "./dto/create-post.dto";
+import { UpdatePostDto } from "./dto/update-post.dto";
+import { DataSource, Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+import { PostEntity } from "./entities/post.entity";
 
 @Injectable()
 export class PostsService {
+  @InjectRepository(PostEntity) private postRepository: Repository<PostEntity>;
+  constructor(private dataSource: DataSource) {}
+
   create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+    return "This action adds a new post";
   }
 
   findAll() {
     return `This action returns all posts`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  async findOne(id: number, category: string): Promise<object> {
+    // if (category === "all") {
+    const post = await this.postRepository.find();
+    // }
+
+    return post;
   }
 
   update(id: number, updatePostDto: UpdatePostDto) {
